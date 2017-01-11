1fd1e82699fbcfb04d008e7298f73db6e8a0e560
Olivier Goffart <ogoffart@woboq.com>
2017-01-11 15:55:07 +0100

    SelectiveSync: adjust the position of the "Loading" label
    It should be parented to the treeview to be sure that is shows inside of it.

--- src/gui/selectivesyncdialog.cpp
+++ src/gui/selectivesyncdialog.cpp
@@ -64,7 +64,7 @@ SelectiveSyncWidget::SelectiveSyncWidget(AccountPtr account, QWidget *parent)
     , _bigFolderSizeLimitBytes(-1)
     , _folderTree(new QTreeWidget(this))
 {
-    _loading = new QLabel(tr("Loading ..."), this);
+    _loading = new QLabel(tr("Loading ..."), _folderTree);
 
     auto layout = new QVBoxLayout(this);
     layout->setContentsMargins(0, 0, 0, 0);
